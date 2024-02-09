d5e939e4a99eeacdafbab79494cae3d84d53d0a0
Junio Dias <9145junio@gmail.com>
2024-02-09 13:32:42 -0300

    junin

---
+++ javascriptaulas/segundoscript.js
@@ -0,0 +1,18 @@
+var j = 3;
+
+var d = 2;
+
+console.log(j*d)
+console.log(j/d)
+console.log(j+d)
+console.log(j-d)
+
+// console.log("j*d",j*d)
+// console.log("j/d",j/d)
+// console.log("j+d",j+d)
+// console.log("j-d",j-d)  Caso queira imprimir na tela a operação escreva em parenteses uma parte, e deixe a outra de forma normal, ou seja faça uma copia.
+
+
+console.log(21 > 12)
+
+console.log(19 < 13)
