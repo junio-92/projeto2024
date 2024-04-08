2209d6bab781cc47b2d4a66b1eeab4a9820b7032
Junio Dias <9145junio@gmail.com>
2024-04-08 19:26:20 -0300

    renovacao

--- javascriptaulas/primeiroscript.js
+++
@@ -1,4 +0,0 @@
-//alert("Olá, Programador Junio Dias de Oliveira")
-// o alert mostra a mensagem na tela, porem o console.log não mostra, vc só conseguira ver essa mensagem na partede inspecionar
-
-console.log("Olá, Programador Junio Dias de Oliveira")
