917be0437303081afcfc104207aeb3e622ac541d
Junio Dias <9145junio@gmail.com>
2024-04-16 12:44:35 -0300

    feito

--- javascriptaulas/decimoscript.js
+++ javascriptaulas/decimoscript.js
@@ -5,7 +5,7 @@ var posicao = document.getElementById("titulo");
 // getElementByTagName
 var paragrafo = document.getElementsByTagName("p")
 
-console.log(paragrafo[0].innerText)
+console.log(paragrafo[1].innerText)
 
 // getElentByClassName
 
