1abde8a1da5976f269a44103af405c385ee90953
Junio Dias <9145junio@gmail.com>
2024-04-16 12:17:54 -0300

    rev

---
+++ javascriptaulas/decimoscript.js
@@ -0,0 +1,14 @@
+///////////// DOM ///////////
+
+var posicao = document.getElementById("titulo");
+
+// getElementByTagName
+var paragrafo = document.getElementsByTagName("p")
+
+console.log(paragrafo[0].innerText)
+
+// getElentByClassName
+
+// querySelectorAll
+
+
