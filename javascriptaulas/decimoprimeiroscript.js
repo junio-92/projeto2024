4ad820dbfd6389034d967065069f7d6a4894a74c
Junio Dias <9145junio@gmail.com>
2024-04-18 22:49:14 -0300

    canvas

---
+++ javascriptaulas/decimoprimeiroscript.js
@@ -0,0 +1,13 @@
+var canvas = document.getElementById("artes");
+
+var ctx = canvas.getContext('2d');
+
+var x = 20;
+
+var y = 20;
+
+var altura = 100;
+
+var largura =100;
+
+ctx.fillRect(x, y, largura, altura)
