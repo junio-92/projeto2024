d2f9b48656922e822edc0bcd003accd4910b2293
Junio Dias <9145junio@gmail.com>
2024-04-20 10:35:20 -0300

    alterar

--- javascriptaulas/decimoprimeiroscript.js
+++ javascriptaulas/decimoprimeiroscript.js
@@ -10,4 +10,19 @@ var altura = 100;
 
 var largura =100;
 
+ctx.fillStyle = "orange"
+
 ctx.fillRect(x, y, largura, altura)
+
+//ctx.fillStyle = "rgba(0,0,200,0.5"  caso queira deixar a imagem transparente se utiliza está função.
+
+ctx.fillStyle = "green"
+ctx.fillRect(30, 30, 100,100)
+
+
+ctx.beginPath();
+
+
+ctx.arc(150, 150, 50, 0, Math.PI*2);
+ctx.stroke();
+
