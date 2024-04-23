1d1c9632a79329edba290ca43a1848384d506045
Junio Dias <9145junio@gmail.com>
2024-04-22 23:56:39 -0300

    objeto

---
+++ javascriptaulas/decimosegundoscript.js
@@ -0,0 +1,26 @@
+//// ANIMAÇÃO /////
+
+let canvas = document.getElementById("andar");
+let ctz = canvas.getContext('2d');
+
+
+// Configurações //
+
+let x = 0;
+let y = 0;
+
+let largura = 100;
+let altura = 100;
+
+/// Animação ///
+
+
+
+function animado(){
+    ctz.clearRect(0, 0, canvas.width,canvas.heigth)
+    ctz.fillRect(x++, y++, largura++, altura++)
+
+}
+
+setInterval(animado, 100)
+
