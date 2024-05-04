86d461b4b09a346093cd7c13a0c602783b50fffe
Junio Dias <9145junio@gmail.com>
2024-05-03 21:36:31 -0300

    api

--- javascriptaulas/decimoquartoscript.js
+++ javascriptaulas/decimoquartoscript.js
@@ -6,7 +6,7 @@ let xhr = new XMLHttpRequest();
 
 let i = 0;
 
-var doc = {
+let doc = {
     "userId":1,
     "title":"Olá",
     "body":"Olá Junio Dias "
@@ -30,3 +30,11 @@ xhr.onreadystatechange = ()=>{
 
 xhr.open("POST", "https://jsonplaceholder.typicode.com/posts");
 xhr.send(doc);
+
+/*
+200 (tudo ok).
+401 (acesso não autorizado).
+403 (acesso proibido).
+404 (page not found: página não encontrada).
+500 (erro interno no servidor)
+/*
--- javascriptaulas/decimosextoscript.js
+++ javascriptaulas/decimosextoscript.js
@@ -1,40 +1,48 @@
-function buscarCEP(){
- 
-    // entrada
-    var entrada=document.getElementById('buscar_cep').value;
-   
-    // saída
-    var cep = document.getElementById('cep');
-    var logradouro = document.getElementById('logradouro');
-    var bairro = document.getElementById('bairro');
-    var cidade = document.getElementById('cidade');
-    var estado = document.getElementById('estado');
-   
-    // cria objeto xhr
-    var xhr = new XMLHttpRequest(); 
-    xhr.responseType = "json"; 
-   
-    // envia requisição
-    xhr.open("GET", 
-      "https://viacep.com.br/ws/"+entrada+"/json/"); 
-    xhr.send();  
-   
-    // verifia se o estado da conexão mudou
-    xhr.onreadystatechange = ()=>{
-   
-      if(xhr.readyState == 4 && xhr.status == 200){
-   
-        // mostra resultado na tela
-        console.log(xhr.response);
+function buscarCEP() {
+
+  //Entrada
+
+  let entrada=document.getElementById('buscar_cep').value;
+
+  //Saída
+
+  let cep = document.getElementById('cep');
+  let logradouro =document.getElementById('logradouro');
+  let bairro = document.getElementById('bairro');
+  let cidade = document.getElementById('cidade');
+  let estado = document.getElementById('estado');
+
+  // Criar objeto xhr
+
+  let xhr = new XMLHttpRequest();
+  xhr.responseType = "json";
+
+  //Envia requisição
+
+  xhr.open("GET", "https://viacep.com.br/ws/"+entrada+"/json/");
+  xhr.send();
+
+  //Verifica se o estado da conecxão mudou
+
+  xhr.onreadystatechange = ()=>{
+
+    if (xhr.readyState == 4 && xhr.status == 200) {
+
+      //Mostra resultado da tela
+
+      console.log(xhr.response);
+
+      //Grava valores
+
+      cep.innerText = xhr.response.cep;
+      logradouro.innerText = xhr.response.logradouro;
+      bairro.innerText = xhr.response.bairro;
+      cidade.innerText = xhr.response.localidade;
+      estado.innerText = xhr.response.uf
       
-        // grava valores
-        cep.innerText = xhr.response.cep;
-        logradouro.innerText = xhr.response.logradouro;
-        bairro.innerText = xhr.response.bairro;
-        cidade.innerText = xhr.response.localidade;
-        estado.innerText = xhr.response.uf;
-      }
     }
-   
   }
+
+
   
+}
